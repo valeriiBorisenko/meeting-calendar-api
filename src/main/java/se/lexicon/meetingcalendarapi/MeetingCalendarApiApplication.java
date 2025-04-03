bda2d853350eda7929646c076f1cda00e5e9f409
Valerii Borisenko <valerii.borisenko@gmail.com>
2025-04-03 09:58:59 +0200

    init project

---
+++ src/main/java/se/lexicon/meetingcalendarapi/MeetingCalendarApiApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.meetingcalendarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingCalendarApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingCalendarApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/meetingcalendarapi/MeetingCalendarApiApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.meetingcalendarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingCalendarApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
